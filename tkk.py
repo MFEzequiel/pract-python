d0b785e8546527baefdd01f61b66806ee22f2293
Marcelo <marceloexequielf4@gmail.com>
2023-11-09 20:49:42 -0300

    tkk

--- tkk.py
+++ tkk.py
@@ -1,4 +1,18 @@
-print("ingresa tu edad")
-age = int(input())
-year = 2023
-print(age-year)
+import tkinter as tk
+import tkinter as ttk
+
+# Funciones
+
+def cal():
+    pass
+
+# Crear una root
+root = tk.Tk()
+root.title("App")
+
+
+
+
+
+# Inicializar la root
+root.mainloop()
